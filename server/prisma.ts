c1ec18b0db20d344d138a716033b326e6f970c4e
Li Arolf Rey <liarolfrrey@gmail.com>
2021-08-04 14:37:12 +0800

    fix: Fix prisma too many clients running bug on dev

--- server/prisma.ts
+++ server/prisma.ts
@@ -1,3 +1,24 @@
+/* eslint-disable no-unused-vars */
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient()
+declare global {
+  namespace NodeJS {
+    interface Global {
+      prisma: PrismaClient
+    }
+  }
+}
+
+let prisma: PrismaClient
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+
+  prisma = global.prisma
+}
+
+export { prisma }
